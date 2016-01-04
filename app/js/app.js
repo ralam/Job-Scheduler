e0bb503dd2bef3ffd058316af55e34ecc8aa5a95
ralam <rehan.alam@gmail.com>
2016-01-03 20:28:03 -0800

    clean up angular-seed files

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,12 @@
-'use strict';
+(function 9) {
+  'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+  // Declare app level module which depends on views, and components
+  var jobScheduler = angular.module('myApp', [
+    'ngRoute',
+    'jobSchedulerControllers'
+  ]).
+  config(['$routeProvider', function($routeProvider) {
+    $routeProvider.otherwise({redirectTo: '/index'});
+  }]);
+}
